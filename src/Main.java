2c1aea907e4261be13caf2299710d4a7bdecc9b7
Mark Mesander <mark_mesander@hotmail.com>
2023-08-29 14:26:01 +0200

    Add name scanner and inputcurrency scanner

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,50 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner inputName = new Scanner(System.in);
+        System.out.println(" ");
+        System.out.println("Super Ultra Mega Currency Exchanger Deluxe");
+        System.out.println("Enter your name: ");
+        System.out.println("--------------------------------------------");
+
+        String name = inputName.nextLine();
+
+        String inputCurrency = null;
+        boolean inputCurrencyLoop = true;
+
+        String inputToCurrency = null;
+
+        System.out.println(" ");
+        System.out.println("Welcome " + name);
+        System.out.println("Which currency would you like to exchange?");
+        System.out.println("You can choose between Dollar, Euro, Pound, Yuan, Roebel, Baht");
+        System.out.println("Enter your currency or enter 'Quit' to stop the program: ");
+        System.out.println("--------------------------------------------");
+
+        while (inputCurrencyLoop) {
+            String[] currencies = {"Dollar", "Euro", "Pound", "Yuan", "Roebel", "Baht", "Quit"};
+            Scanner scanCurrency = new Scanner(System.in);
+            inputCurrency = scanCurrency.nextLine();
+
+            for (int i = 0; i < currencies.length; i++) {
+                if (inputCurrency.equalsIgnoreCase(currencies[i])) {
+                    inputCurrencyLoop = false;
+                    break;
+                } else if (i == 5) {
+                    System.out.println("This is not a valid currency. Please choose between Dollar, Euro, Pound, Yuan, Roebel, Baht");
+                    System.out.println("Enter your currency or enter 'Quit' to stop the program: ");
+                } else if (inputCurrency.equalsIgnoreCase(currencies[6])) {
+                    System.out.println("Thank you " + name + " for using this program.");
+                    System.out.println("And don't forget, you're awesome " + name);
+                    System.out.println("--------------------------------------------");
+                    inputCurrencyLoop = false;
+                    break;
+                }
+            }
+
+        }
+
     }
 }
